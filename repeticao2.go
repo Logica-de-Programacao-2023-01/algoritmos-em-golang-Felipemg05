8263dce996084d5e440ac07378a9418502207dac
Felipe Madruga Gusmão <felipe.mg@dinfor.uniceub>
2023-03-21 10:47:49 -0300

    solucao

--- repeticao2.go
+++ repeticao2.go
@@ -1,6 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	for i :=0;
+	for i := 0; i < 20; i++ {
+		if i%2 != 1 {
+			fmt.Println(i)
+		}
+	}
 
 }
---
+++ sequencial1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero1 int
+	var numero2 int
+	var numero3 int
+	fmt.Print("Qual é o valor do primeiro número?")
+	fmt.Scan(&numero1)
+	fmt.Println("Valor do primeiro número:", numero1)
+}
