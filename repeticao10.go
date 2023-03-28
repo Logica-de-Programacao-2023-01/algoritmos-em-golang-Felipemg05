20b5f4571e7f6d6169b40a2061d38792557167d6
Felipe Madruga Gusmão <felipe.mg@dinfor.uniceub>
2023-03-28 08:14:34 -0300

    solucoes

---
+++ repeticao10.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var maior, x  int
+
+
+	fmt.Println("Digite um número:")
+	fmt.Scan(&x)
+
+	maior = x
+
+	for x != 0 {
+		fmt.Print("Digite um número:")
+		fmt.Scan(&x)
+
+		if x maior
+	}
+}
---
+++ repeticao9.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var soma, quantidade, x int
+
+	fmt.Print("Insira um número:")
+	fmt.Scan(&x)
+
+	for x != 0 {
+		fmt.Print("Insira um número:")
+		fmt.Scan(&x)
+
+		if x != 0 {
+
+		}
+	}
+}
