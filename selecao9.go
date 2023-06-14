18e56d0601b4031b5b8606a4899df1d14cebc6c8
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-14 18:51:47 -0300

    solucao

---
+++ selecao9.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "sort"
+
+func main() {
+    var num1, num2, num3 float64
+
+    fmt.Print("Digite o primeiro número real: ")
+    fmt.Scan(&num1)
+
+    fmt.Print("Digite o segundo número real: ")
+    fmt.Scan(&num2)
+
+    fmt.Print("Digite o terceiro número real: ")
+    fmt.Scan(&num3)
+
+    numeros := []float64{num1, num2, num3}
+    sort.Float64s(numeros)
+
+    fmt.Println("Números em ordem crescente:", numeros)
+}
